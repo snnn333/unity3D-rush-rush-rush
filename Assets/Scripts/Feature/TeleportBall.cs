1658ea19d88d3e0346f1aad4c488d6403219a5ef
fanzhangg <vanadiumzhang@gmail.com>
2022-09-15 00:50:40 -0700

    Modify the stage 1 of level1

---
+++ Assets/Scripts/Feature/TeleportBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformCharacterController
+{
+    public class TeleportBall : MonoBehaviour
+    {
+        Vector3 m_StartPosition;
+
+        void Start()
+        {
+            m_StartPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            if (transform.position.y < -5)
+            {
+                transform.position = new Vector3(3F, 5.5F, -10.5F);
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            }
+        }
+    }
+}
