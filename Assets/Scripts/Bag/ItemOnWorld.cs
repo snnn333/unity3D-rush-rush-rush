f7f4002c1281268b7a3a7e99d6244d9c8255b661
hemings <sun201709@outlook.com>
2022-11-07 20:31:58 -0800

    add text popup when near item, half done

--- Assets/Scripts/Bag/ItemOnWorld.cs
+++ Assets/Scripts/Bag/ItemOnWorld.cs
@@ -14,25 +14,62 @@ public class ItemOnWorld : MonoBehaviour
         // If we don't have this item
         // We need to iterate to find empty position
         if(other.gameObject.CompareTag("Player")){
-            StatisticManager.AddItem(thisItem.name, Time.time);
-            
-            // consume bread
-            if(thisItem.name == "Bread"){
-                Health health = other.gameObject.GetComponent<Health>();
-                if(health.maxHealth > health.currentHealth){
-                    health.currentHealth += 1;
-                    Destroy(this.gameObject);
-                    return;
-                }
-            }else{
-                // add item
-                DisplayMessage(itemMessage + thisItem.name);
-                myBag.AddItem(thisItem);
-                Destroy(this.gameObject);
+
+            AddPopup(other);
+            if(Input.GetKeyDown(KeyCode.E)){
+                AddItem(other);
             }
+            // StatisticManager.AddItem(thisItem.name, Time.time);
+            
+            // // consume bread
+            // if(thisItem.name == "Bread"){
+            //     Health health = other.gameObject.GetComponent<Health>();
+            //     if(health.maxHealth > health.currentHealth){
+            //         health.currentHealth += 1;
+            //         Destroy(this.gameObject);
+            //         return;
+            //     }
+            // }else{
+            //     // add item
+            //     DisplayMessage(itemMessage + thisItem.name);
+            //     myBag.AddItem(thisItem);
+            //     Destroy(this.gameObject);
+            // }
         }   
     }
 
+    private void AddPopup(Collider other){
+        var popupManager = other.GetComponent<TextPopUp>();
+        string text = "Press 'E' to pick up "+ thisItem.name;
+        if(popupManager != null){
+            if(popupManager.hasPopup() && popupManager.getText() == text){
+                popupManager.updatePopup();
+            }else{
+                popupManager.createPopup(text,other.transform.position,other.transform.rotation);
+            }
+        }
+    }
+
+
+    private void AddItem(Collider other){
+        StatisticManager.AddItem(thisItem.name, Time.time);
+        // consume bread
+        if(thisItem.name == "Bread"){
+            Health health = other.gameObject.GetComponent<Health>();
+            if(health.maxHealth > health.currentHealth){
+                health.currentHealth += 1;
+                Destroy(this.gameObject);
+                return;
+            }
+        }else{
+            // add item
+            DisplayMessage(itemMessage + thisItem.name);
+            myBag.AddItem(thisItem);
+            Destroy(this.gameObject);
+        }
+    }
+
+    
     private void DisplayMessage(string msg){
         var message = GameObject.FindGameObjectsWithTag("MessageCenter");
         if(message != null && message.Length > 0){
