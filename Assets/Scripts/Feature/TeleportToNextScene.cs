f5b32b5eb4c64d7f62736e1f700e5b238a88c53c
fanzhangg <vanadiumzhang@gmail.com>
2022-11-13 15:11:55 -0800

    Change the art style and level selection method in level selection scene
    - Make the level look better
    - Now the player needs to press E to enter the level

--- Assets/Scripts/Feature/TeleportToNextScene.cs
+++ Assets/Scripts/Feature/TeleportToNextScene.cs
@@ -7,17 +7,44 @@ namespace PlatformCharacterController
 {
     public class TeleportToNextScene : MonoBehaviour
     {
-        public GameObject gamePassObj;
+        public bool IsNeedKeyPress;
+        private bool _IsEntered = false;
         
         [Tooltip("Name of the next scene.")]
         public string scenename;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player") && IsNeedKeyPress == false)
             {
                 Debug.Log("sceneName to load: " + scenename);
                 SceneManager.LoadScene(scenename);
+            }
+            else if (other.CompareTag("Player") && IsNeedKeyPress == true)
+            {
+                _IsEntered = true;
+                DisplayMessage("Press [E] to Enter Level");
+                Debug.Log("Needs key press to enter the level");
+            }
+        }
+
+        private void Update()
+        {
+            if (_IsEntered && Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(scenename);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _IsEntered = false;
+        }
+
+        private void DisplayMessage(string msg){
+            var message = GameObject.FindGameObjectsWithTag("MessageCenter");
+            if(message != null && message.Length > 0){
+                message[0].GetComponent<Notification>().setMessage(msg);
             }
         }
         
