644243ba1c0b3e3d3648239785e4e284248ac9ed
fanzhangg <vanadiumzhang@gmail.com>
2022-11-19 15:46:38 -0800

    Set a lift height limit to the whirlwind

--- Assets/Scripts/Tools/WindArea.cs
+++ Assets/Scripts/Tools/WindArea.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace PlatformCharacterController
 {
@@ -18,13 +19,16 @@ namespace PlatformCharacterController
         {
             if (other.gameObject.tag == "Player")
             {
-                Debug.Log("Player enters wind area");
                 player = other.gameObject;
                 inWindZone = true;
                 controller = player.GetComponent<MovementCharacterController>();
 
                 // Lift up the player
+                controller.IsLifting = true; // Enable lift
                 controller.Lift(strength, direction);
+
+                // Stop the player from lifting after a few seconds
+                // StartCoroutine(WaitThenStop());
             }
         }
 
@@ -37,12 +41,28 @@ namespace PlatformCharacterController
 
         private void FixedUpdate()
         {
+            // Out of the windzone when the player flies to high away from the fan
+            if (inWindZone == true && Vector3.Distance(transform.position, player.transform.position) > 15f) 
+            {
+                float distance =  Vector3.Distance(transform.position, player.transform.position);
+                string debugMessage = String.Format("Player is too high, out of wind zone. Distance: {0}", distance);
+                Debug.Log(debugMessage);
+                inWindZone = false;
+                controller.IsLifting = false;
+            }
+
             if (inWindZone)
             {
-                Debug.Log("Apply force to the player in the wind zeon");
-                // // Lift up the player
+                // Lift up the player
                 controller.Lift(strength, direction);
             }
+        }
+
+        private IEnumerator WaitThenStop()
+        {
+            yield return new WaitForSeconds(1);
+            inWindZone = false;
+
         }
 
     }
