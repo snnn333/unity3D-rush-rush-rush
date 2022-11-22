e04b3dee8d6f516199f3a42ed4c9f9bfd126da68
NingHao He <he1123783096@outlook.com>
2022-11-22 13:54:48 -0800

    sound effect added

---
+++ Assets/playSound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip coinSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(coinSound, 0.9f*Camera.main.transform.position + 0.1f*transform.position ,10f);
+        }
+    }
+}
