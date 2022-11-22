de1c7e3fcebe7489b6ef4e9da9c1eb211593e061
fanzhangg <vanadiumzhang@gmail.com>
2022-11-21 16:27:49 -0800

    Modify the jumping pad of level 2

--- Assets/Scripts/Feature/ChangeSpeedZone.cs
+++ Assets/Scripts/Feature/ChangeSpeedZone.cs
@@ -9,13 +9,42 @@ namespace PlatformCharacterController
         [Tooltip("This is the speed for the player in this zone")]
         public float ZoneSpeed;
 
+        // Effects
+        public GameObject DashingEffect;
+        public GameObject RunningEffect;
+        private MovementCharacterController _movementController;
+        private bool _isSlowingDown = false;
+        private float _actualSpeed;
+        private GameObject _player;
+
+        private void SpawnEffectOnPlayer(GameObject myVFX)
+    {
+        if (myVFX != null && _player) {
+            GameObject spawnedVFX = Instantiate(myVFX, _player.transform.position, _player.transform.rotation) as GameObject; 
+            // Destroy(spawnedVFX, 5f);
+        }
+    }
+
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                other.GetComponent<MovementCharacterController>().ChangeSpeed(ZoneSpeed);
+                _movementController = other.GetComponent<MovementCharacterController>();
+                _player = other.gameObject;
+                _movementController.ChangeSpeed(ZoneSpeed);
+                _actualSpeed = ZoneSpeed;
+                SpawnEffectOnPlayer(DashingEffect);
+                // SpawnEffectOnPlayer(RunningEffect);
+                // _movementController.DeactivatePlayerControlByTime(10);
+                // StartCoroutine(WaitAndSlowingDown());
             }
+        }
+
+        private IEnumerator WaitAndSlowingDown() {
+            yield return new WaitForSeconds(2);
+            // _isSlowingDown = true;
+            // Destroy(RunningEffect);
         }
 
         private void OnTriggerExit(Collider other)
@@ -23,7 +52,22 @@ namespace PlatformCharacterController
             if (other.CompareTag("Player"))
             {
                 other.GetComponent<MovementCharacterController>().ResetOriginalSpeed();
+                // _isSlowingDown = false;
             }
         }
+
+        // private void Update() {
+        //     // Regress the dashing speed
+        //     if (_isSlowingDown && _actualSpeed > _movementController._originalRunningSpeed) {
+        //         _actualSpeed -= 0.1f;
+        //         _movementController.ChangeSpeed(_actualSpeed);
+        //     }
+
+        //     if (_isSlowingDown && _actualSpeed <= _movementController._originalRunningSpeed) {
+        //         _movementController.ResetOriginalSpeed();
+        //         _isSlowingDown = false;
+        //     }
+            
+        // }
     }
 }
