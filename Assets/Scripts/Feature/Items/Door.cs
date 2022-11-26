fb63db7cf26e9c5a86b422df010c96000c80462b
fanzhangg <vanadiumzhang@gmail.com>
2022-11-25 23:35:15 -0800

    Add a diamond pop up to the level selection scene

--- Assets/Scripts/Feature/Items/Door.cs
+++ Assets/Scripts/Feature/Items/Door.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     public GameObject gamePassObj;
     public int passLevel;
     public Bag bag;
+
+    public Item diamond;
     
     
     private void OnTriggerEnter(Collider other)
@@ -31,5 +34,15 @@ public class Door : MonoBehaviour
 
         // GameObject bagButton = GameObject.FindWithTag("BagButton");
         // bagButton.SetActive(false);
+        
+        // Save the diamond count
+        if (diamond == null) return;
+        string sceneName = SceneManager.GetActiveScene().name;
+        int prevMaxDiamondCount = PlayerPrefs.GetInt(sceneName + "-MaxDiamondCount", 0);
+        if (diamond.num > prevMaxDiamondCount) {
+            Debug.Log("Update the max diamond count of " + sceneName + "from" + prevMaxDiamondCount + "to" + diamond.num);
+            PlayerPrefs.SetInt(sceneName + "-MaxDiamondCount", diamond.num);
+            PlayerPrefs.Save();
+        }
     }
 }
